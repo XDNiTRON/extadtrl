51b0d3d57e7bf587e864bd80080fadc5180621d0
xbeet <84825812+xbeet@users.noreply.github.com>
2021-06-19 22:33:00 +0530

    Create __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -35,23 +35,27 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>Bot Uptime:</b> {currentTime}\n' \
-            f'<b>Start Time:</b> {current}\n' \
-            f'<b>Total Disk Space:</b> {total}\n' \
-            f'<b>Used:</b> {used}  ' \
-            f'<b>Free:</b> {free}\n\n' \
-            f'📊Data Usage📊\n<b>Upload:</b> {sent}\n' \
-            f'<b>Download:</b> {recv}\n\n' \
-            f'<b>CPU:</b> {cpuUsage}%\n' \
-            f'<b>RAM:</b> {memory}%\n' \
-            f'<b>DISK:</b> {disk}%'
+    stats = f'<b>╭───┃🎖BOT  STATiSTiCS🎖┃</b>\n' \
+            f'<b>│</b>\n' \
+            f'<b>├⏱BotUptime:</b> {currentTime}\n' \
+            f'<b>╰⏰StartTime:</b> {current}\n\n' \
+            f'<b>╭─🧰Total DiskSpace:</b> {total}\n' \
+            f'<b>├─📮Used:</b> {used}\n' \
+            f'<b>╰─🏮Free:</b> {free}\n\n' \
+            f'<b>╭───┃🕹 DATA USAGE 🕹┃</b>\n<b>├─🔺Upload:</b> {sent}\n' \
+            f'<b>├─🔻Download:</b> {recv}\n' \
+            f'<b>├─📟CPU:</b> {cpuUsage}%\n' \
+            f'<b>├─💾RAM:</b> {memory}%\n' \
+            f'<b>├─💿DISK:</b> {disk}%\n' \
+            f'<b>│</b>\n' \
+            f'<b>╰─┃🏆@ANonYmoUS_FriEND🏆┃</b>'
     update.effective_message.reply_photo(IMAGE_URL, stats, parse_mode=ParseMode.HTML)
 
 
 def start(update, context):
     start_string = f'''
-This bot can mirror all your links to Google Drive!
-Type /{BotCommands.HelpCommand} to get a list of available commands
+This bot can mirror all your links to Google Drive 💠
+Type /{BotCommands.HelpCommand} to get a list of available commands 🤙
 '''
     buttons = button_build.ButtonMaker()
     buttons.buildbutton("Repo", "https://github.com/breakdowns/slam-mirrorbot")
